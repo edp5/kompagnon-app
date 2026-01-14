dd9003e78d9ff53bad743f45c38347b67521acf8
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-01-14 10:36:38 +0100

    fix: fix the eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -6,51 +6,81 @@ import globals from "globals";
 
 export default defineConfig([
   expoConfig,
+
   {
-    ignores: ["dist/*"],
+    ignores: ["dist/**"],
   },
+
   {
     files: ["**/*.{js,mjs,cjs}"],
+
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".mjs", ".cjs"],
+        },
+      },
+    },
+
+
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
+
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"],
-      indent: ["error", 2],
       "comma-dangle": ["error", "always-multiline"],
       "comma-spacing": ["error", { before: false, after: true }],
       "object-curly-spacing": ["error", "always"],
+
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+
       "padding-line-between-statements": [
         "error",
         { blankLine: "always", prev: "block", next: "block" },
         { blankLine: "always", prev: "function", next: "function" },
         { blankLine: "always", prev: "class", next: "function" },
       ],
-      "prefer-const": ["error"],
-      "space-before-blocks": ["error"],
+
+      "prefer-const": "error",
+
+      "space-before-blocks": "error",
       "space-before-function-paren": [
         "error",
-        { anonymous: "never", named: "never", asyncArrow: "ignore" },
+        {
+          anonymous: "never",
+          named: "never",
+          asyncArrow: "always",
+        },
       ],
-      "space-in-parens": ["error"],
-      "space-infix-ops": ["error"],
-      "func-call-spacing": ["error"],
-      "key-spacing": ["error"],
-      "no-trailing-spaces": ["error"],
-      "no-multi-spaces": ["error"],
-      "func-style": ["error", "declaration", { allowArrowFunctions: false }],
+      "space-in-parens": ["error", "never"],
+      "space-infix-ops": "error",
+      "func-call-spacing": ["error", "never"],
+      "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+      "no-trailing-spaces": "error",
+      "no-multi-spaces": "error",
+
+      "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["^@env$"],
+        },
+      ],
     },
+
     languageOptions: {
       globals: {
         ...globals.node,
       },
     },
   },
+
   {
-    files: ["**/*.test.{js,mjs,ts,tsx}", "**/*.spec.{js,mjs,ts,tsx}"],
+    files: ["**/*.test.{js,mjs}", "**/*.spec.{js,mjs}"],
     languageOptions: {
       globals: {
         ...globals.jest,
